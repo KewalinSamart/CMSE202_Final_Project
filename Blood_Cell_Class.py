021c4ee87c82ae2dc2841178c6389fc57dcbf7da
Kewalin Samart <samartke@msu.edu>
2021-04-25 21:58:41 -0400

    Update Blood_Cell_Class.py

--- Blood_Cell_Class.py
+++ Blood_Cell_Class.py
@@ -5,11 +5,13 @@ class blood_cell():
     This is the blood cells class and it allows the blood cells to move around as well as absorb oxygen and carbon dioxide.
     '''
     
-    def __init__(self, x_dims, y_dims, shape='round'): #We could change the shape attribute to an oxygen capacity attribute
+    def __init__(self, dims, shape='round', color = 'red'): #We could change the shape attribute to an oxygen capacity attribute
         self.shape = shape
         #self.molecule = molecule
-        self.x_dim = random.randint(x_dims[0], x_dims[1])
-        self.y_dim = random.randint(y_dims[0], y_dims[1])
+        self.coords = dims 
+        self.dims = random.sample(dims, 1)
+        self.x = self.dims[0][0]
+        self.y = self.dims[0][1]
         
         if self.shape == 'round':
             oxygen_cap = 6
@@ -21,6 +23,11 @@ class blood_cell():
         self.oxygen_cap = oxygen_cap
         self.carbon_cap = carbon_cap
         
+        self.color = color 
+        
+        b_x = self.x 
+        b_y = self.y
+        
     def transfer(self):
         # Determine if blood cell is in lungs using nearest neighbor, if it is then it picks up the maximum amount of oxygen and releases
         # CO2
@@ -28,48 +35,85 @@ class blood_cell():
         # Determine if blood cell is in an organ using nearest neighbor, if it is then it picks up the max amount of CO2 and releases
         # oxygen
         #neighbors = getNeighborValues(self.x_dim, self.y_dim, board)
-        if 70 < self.x_dim < 135 and 5 < self.y_dim < 40:
+        if 70 < self.x < 135 and 5 < self.y < 40:
             #ox_supply = self.oxygen_cap
             #carb_supply = 0
             color = 'red'
-        elif 80 < self.x_dim < 120 and ((140 < self.y_dim < 160) or (170 < self.y_dim < 190 ) or (200 < self.y_dim < 220)):
+            self.color = color
+            
+        elif 80 < self.x < 120 and ((140 < self.y < 160) or (170 < self.y < 190 ) or (200 < self.y < 220)):
             #ox_supply = 0
             #carb_supply = self.carbon_cap
             color = 'blue'
-            
-        self.color = color
+            self.color = color
         
-    def draw(self, board):
-        
-        plt.figure(figsize=(20,10))
-        plt.imshow(board) 
-        plt.scatter(self.x_dim, self.y_dim, color=self.color)
+    def draw(self):
+         
+        plt.scatter(self.x, self.y, c=self.color)
 
     
-    def move_left(self):
-        # Subtract from the x coordinate, moving 1 unit left, y_coordinate can stay the same
-        dx = -1
-        new_xL = self.x_dim + dx
-        new_yL = self.y_dim # or we could have a tiny range of back and forth y movement in a loop (new_yL = self.y_dim + (-1)^n_steps)
+    def movement_up(self):
         
-        self.x_dim = new_xL
-        self.y_dim = new_yL
-    
-    def move_up(self):
-        # Subtract from y coordinate, moving 1 unit up, x_coordinate can stay the same
-        dy = -1
-        new_xU = self.x_dim  # or we could have a tiny range of back and forth x movement in a loop (new_xU = self.x_dim + (-1)^n_steps)
-        new_yU = self.y_dim + dy 
+        '''
+        Oxygen moves through the arteries towards the heart and the lung
+        This is shown through the oxygen object moving 1 unit upwards
+        '''
+        b_dy = 1
         
-        self.x_dim = new_xU
-        self.y_dim = new_yU
+        b_x = self.x 
+        b_y = self.y - b_dy 
         
-    def move_right(self):
-        # Add to x coordinate, moving 1 unit right, y_coordinate can stay the same
-        dx = 1
-        new_xR = self.x_dim + dx
-        new_yR = self.y_dim # or we could have a tiny range of back and forth y movement in a loop (new_yR = self.y_dim + (-1)^n_steps)
+        self.x = b_x #oxygen coordinates update after it moves 
+        self.y = b_y   
         
-        self.x_dim = new_xR
-        self.y_dim = new_yR
-    
+    def movement_down(self):
+        
+        '''
+        Oxygen moves through the arteries towards organs
+        This is shown through the oxygen object moving 1 unit downwards
+        '''
+        b_dy = 1
+        
+        b_x = self.x 
+        b_y = self.y + b_dy 
+        
+        self.x = b_x #oxygen coordinates update after it moves 
+        self.y = b_y
+        
+    def movement_right(self):
+        
+        '''
+        Oxygen moves through the arteries towards organs  
+        This is shown through the oxygen object moving 1 unit to the right 
+        '''
+        
+        b_dx = 1 
+        
+        b_x = self.x + b_dx
+        b_y = self.y 
+        
+        self.x = b_x #oxygen coordinates update after it moves 
+        self.y = b_y      
+        
+    def movement_left(self):
+        
+        '''
+        Oxygen moves through the arteries towards organs 
+        This is shown through the oxygen object moving 1 unit to the left 
+        '''
+        
+        b_dx = 1
+        
+        b_x = self.x - b_dx
+        b_y = self.y 
+        
+        self.x = b_x #oxygen coordinates update after it moves 
+        self.y = b_y
+        
+    def board_location(self):
+        
+        '''
+        Provides the current location of the oxygen object on the board
+        '''
+        
+        return self.x, self.y
